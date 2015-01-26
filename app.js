869e38ec31eae8c7902b57e57c5957f6da93731a
Adrian Green <adriangreen@Adrians-MacBook-Air.local>
2015-01-25 21:27:18 -0500

    styled and carousel

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var express = require('express'),
 
 var twit = require('twit');
 var T = new twit({
-    consumer_key: '',
-    consumer_secret: '',
-    access_token: '',
-    access_token_secret: ''
+    consumer_key: 'ifYRUgfpEctzwrhVkCnlsGHhZ',
+    consumer_secret: '8mcyW8KAhDhbvu4DTQdvDytMViCCNLRFgjtek5rIuJ6cZeewlo',
+    access_token: '2345694962-YkesAp9P3tVTyt9AdqXJ9nQmMHlFqkIfMGQ5cdS',
+    access_token_secret: 'sw8gz44TBtjmXbct7qNqLqxk208YkSsn7ldOlFBoC4ISE'
 })
 
 var app = express();
@@ -22,15 +22,15 @@ app.get('/', function(req,res){
 app.post('/tweets', function(req,res){
     console.log('in post')
     T.get('search/tweets', {q:'#canjs',count:50}, function(err,data,resp){
-        console.log(data)
+        //console.log(data.statuses)
         var results = []
         var counter = 0;
         for(i in data.statuses){
             counter++
-            console.log(data.statuses[i].text)
+            console.log(data.statuses[i].entities.urls)
             var foo = data.statuses[i];
-            results.push({user:foo.user.name,text:foo.text})
-            console.log(counter)
+            results.push({url:foo.user.url,image:foo.user.profile_image_url,user:foo.user.name,text:foo.text})
+            //console.log(counter)
             if(counter == data.statuses.length){
                 res.send(results)
             }
--- public/app/js/canjs.js
+++ public/app/js/canjs.js
@@ -17,20 +17,20 @@ $('#my-firstcontain').html(frag1)
 
 //get some tweets
 
-
-
-setInterval(function(){
-   $.post('/tweets', {data:'data'},function(data){
+$.post('/tweets', {data:'data'},function(data){
         //tweets.push({text:data.text})
         var tweets = new can.List([])
         
         //$('#tweets').html(frag3);
        
         for(i in data){
-            tweets.push({user:data[i].user,text:data[i].text})
+            tweets.push({url:data[i].url,image:data[i].image,user:data[i].user,text:data[i].text})
         }
        var frag3 = can.view("twitter",{tweet:tweets})
        $('#tweets').html(frag3)
-       console.log(data)
-    }) 
-},5000)
+       //console.log(data)
+    })
+
+/*setInterval(function(){
+    
+},5000)*/
